aae9259b30eacef4d9820440d71b3bbd60b4df9a
정우태 <wjddnxo1478@naver.com>
2021-08-02 01:15:40 +0900

    ViewModel 추가

--- app/src/main/java/com/example/androidchatapp/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/androidchatapp/fragment/LoginFragment.kt
@@ -1,23 +1,19 @@
 package com.example.androidchatapp.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.androidchatapp.R
 import com.example.androidchatapp.databinding.FragmentLoginBinding
+import com.example.androidchatapp.models.LoginViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
+    private val viewModel: LoginViewModel by activityViewModels()
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
@@ -36,23 +32,24 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            loginViewModel = viewModel
+            loginFragment = this@LoginFragment
+        }
+    }
+
+    fun signIn() {
+        Log.d("LoginViewModel", "Email: ${viewModel.email}, password: ${viewModel.password}")
+    }
+
+    fun signUp() {
+        findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
--- app/src/main/java/com/example/androidchatapp/models/LoginViewModel.kt
+++ app/src/main/java/com/example/androidchatapp/models/LoginViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.androidchatapp.models
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.androidchatapp.R
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private lateinit var _navHostFragment: NavHostFragment
+
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+
+    private val _passwordConfirm = MutableLiveData<String>()
+    val passwordConfirm: LiveData<String> = _passwordConfirm
+
+    private val  _employeeNumber = MutableLiveData<Int>()
+    val employeeNumber: LiveData<Int> = _employeeNumber
+
+    fun cancel() {
+
+    }
+
+
 }
